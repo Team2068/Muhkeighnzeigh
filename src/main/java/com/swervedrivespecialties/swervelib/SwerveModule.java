e285b3b8d040b59d264283e5ee34389f21a05666
keiraSchoolcraft <schineaj23@pwcs-edu.org>
2023-01-08 15:34:49 -0500

    Add Modified SDS Library
    SDS looks dead so we had to update it ourselves to use Distances for 2023 wpilib
    
    Co-Authored-By: HannahY437 <78826979+HannahY437@users.noreply.github.com>

---
+++ src/main/java/com/swervedrivespecialties/swervelib/SwerveModule.java
@@ -0,0 +1,11 @@
+package com.swervedrivespecialties.swervelib;
+
+public interface SwerveModule {
+    double getDriveVelocity();
+
+    double getDrivePosition();
+
+    double getSteerAngle();
+
+    void set(double driveVoltage, double steerAngle);
+}

bf4e23508ebf59db2f413e6165c7e26c5021e0ec
AllusionistZ <Lavapointgaming@Gmail.com>
2023-01-09 17:46:13 -0500

    Fixes comments

---
+++ src/main/java/frc/robot/tests/DebugValueTest.java
@@ -0,0 +1,13 @@
+package frc.robot.tests;
+
+import frc.robot.commands.DebugValue;
+// Optional Test for DebugValue located in commands directory. (Can be removed as well as the directory if not wanted)
+public class DebugValueTest {
+    public void Test() {
+        String DataName = "TestValue";
+        System.out.println(DebugValue.get(DataName));
+    
+        DebugValue.set(DataName, 2.0);
+        System.out.println(DebugValue.get(DataName));
+    }
+}

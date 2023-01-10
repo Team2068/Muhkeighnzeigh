e5c3d80da011f7c966d31d46e6a166af367babcf
keiraSchoolcraft <>
2023-01-10 15:37:55 -0500

    save files

--- src/main/java/frc/robot/commands/DebugTable.java
+++ src/main/java/frc/robot/commands/DebugTable.java
@@ -3,7 +3,7 @@ package frc.robot.commands;
 import java.lang.Object;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class DebugValue {
+public class DebugTable {
     static NetworkTableInstance NetTabInst = NetworkTableInstance.getDefault();
     public static Object get(String DataName) {
         return NetTabInst.getTable(DataName).getValue(DataName);

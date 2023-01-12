f5bce121bbd45566c4c0abde7bd99d3721450b98
Andrew Schineller <66565336+TotallyFarhan@users.noreply.github.com>
2023-01-12 15:10:36 -0500

    DebugTable Fix

--- src/main/java/frc/robot/commands/DebugTable.java
+++ src/main/java/frc/robot/commands/DebugTable.java
@@ -1,12 +1,13 @@
 package frc.robot.commands;
 
 import java.lang.Object;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class DebugValue {
-    static NetworkTableInstance NetTabInst = NetworkTableInstance.getDefault();
+public class DebugTable {
+    static NetworkTable NetTabInst = NetworkTableInstance.getDefault().getTable("Debug");
     public static Object get(String DataName) {
-        return NetTabInst.getTable(DataName).getValue(DataName);
+        return NetTabInst.getValue(DataName);
     };
 
     public static void set(String DataName, Object Data) {

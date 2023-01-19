95959d4f1bf226c1a8b1250d45afd555abb95ba1
keiraSchoolcraft <schineaj23@pwcs-edu.org>
2023-01-19 15:23:40 -0500

    fixed path errors
    Co-Authored-By: malikma24 <114297599+malikma24@users.noreply.github.com>

--- src/main/java/frc/robot/commands/Auto1.java
+++ src/main/java/frc/robot/commands/Auto1.java
@@ -7,13 +7,14 @@ import frc.robot.subsystems.DriveSubsystem;
 public class Auto1 extends SequentialCommandGroup {
     public Auto1(DriveSubsystem driveSubsystem) {
         addCommands(
-            new Paths(TrajectoryPaths.Step1_4Cargo, driveSubsystem), 
+            new Paths(Trajectories.Step1_4Cargo, driveSubsystem), 
             new Paths(Trajectories.Step2_4Cargo, driveSubsystem),
-            new Paths(TrajectoryPaths.Step3_4Cargo, driveSubsystem),
-            new Paths(TrajectoryPaths.Step4_4Cargo, driveSubsystem),
-            new Paths(TrajectoryPaths.Step5_4Cargo, driveSubsystem),
-            new Paths(TrajectoryPaths.Step5_4Cargo, driveSubsystem),
-            new Paths(TrajectoryPaths.Step6_4Cargo, driveSubsystem)
+            new Paths(Trajectories.Step3_4Cargo, driveSubsystem),
+            new Paths(Trajectories.Step4_4Cargo, driveSubsystem),
+            new Paths(Trajectories.Step5_4Cargo, driveSubsystem),
+            new Paths(Trajectories.Step5_4Cargo, driveSubsystem),
+            new Paths(Trajectories.Step6_4Cargo, driveSubsystem)
+            //Scenario 2 
     );
 }
 }
--- src/main/java/frc/robot/commands/Paths.java
+++ src/main/java/frc/robot/commands/Paths.java
@@ -1,21 +1,76 @@
 package frc.robot.commands;
 
+import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+
 import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Constants.Trajectories;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class Paths extends CommandBase {
+
     private final Timer m_timer = new Timer();
     private final Trajectory m_trajectory;
     private final Pose2d m_pose;
     private final SwerveDriveKinematics m_kinematics;
     private final HolonomicDriveController m_controller;
-    private final DriveSubsystem m_drivetrain;
-    
-    
-    
+    private final DriveSubsystem m_drivet;
+
+    @SuppressWarnings("ParameterName")
+    public Paths(Trajectory trajectory, DriveSubsystem drivetrainSubsystem){
+        this.m_drivet = drivetrainSubsystem;
+        this.m_trajectory = trajectory;
+        this.m_kinematics = drivetrainSubsystem.getKinematics();
+        this.m_pose = drivetrainSubsystem.getPose();
+
+        ProfiledPIDController thetaController = new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        m_controller = new HolonomicDriveController(
+                new PIDController(AutoConstants.kPXController, 0, 0),
+                new PIDController(AutoConstants.kPYController, 0, 0),
+                thetaController
+        );
+        
+        addRequirements(m_drivet);
+    }
+    @Override
+    public void initialize() {
+        m_drivetrain.resetOdometryWithPose2d(m_trajectory.getInitialPose());
+        m_timer.reset();
+        m_timer.start();
+    }
+
+    @Override
+    @SuppressWarnings("LocalVariableName")
+    public void execute() {
+        double curTime = m_timer.get();
+        var desiredState = (PathPlannerState) m_trajectory.sample(curTime);
+
+        var targetChassisSpeeds = m_controller.calculate(m_drivetrain.getPose(), desiredState, desiredState.holonomicRotation);
+        // var targetModuleStates = m_kinematics.toSwerveModuleStates(targetChassisSpeeds);
+        DriverStation.reportWarning("TARGET: " + desiredState.poseMeters.toString() + "\n ACTUAL: " + m_pose.toString(), false);
+
+        m_drivetrain.drive(targetChassisSpeeds);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_timer.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return m_timer.hasElapsed(m_trajectory.getTotalTimeSeconds());
+    }
 }
+
+

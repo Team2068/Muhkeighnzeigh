34c701011a2a60e031e63d2188b40514c9320c4e
keiraSchoolcraft <schineaj23@pwcs-edu.org>
2023-02-02 16:34:14 -0500

    Adding profiled PID to arm
    Need to add in encoder commands and edit feedforward, also need to edit the pid values

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,38 +1,41 @@
 package frc.robot.subsystems;
 
+import java.beans.Encoder;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 
-public class ArmSubsystem extends SubsystemBase{
+public class ArmSubsystem extends ProfiledPIDSubsystem{
     private CANSparkMax ArmMotor1 = new CANSparkMax(ArmConstants.ArmMotor1, MotorType.kBrushless);
     private CANSparkMax ArmMotor2 = new CANSparkMax(ArmConstants.ArmMotor2, MotorType.kBrushless);
 
+    private final ArmFeedforward armFeedforward = 
+    new ArmFeedforward(0, 0, 0, 0);
     public ArmSubsystem(){
-        ArmMotor1.setIdleMode(IdleMode.kCoast);
-        ArmMotor2.setIdleMode(IdleMode.kCoast);
+   super(
+    new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(0, 0))
+   );
+
+   
+}
+    @Override
+    protected void useOutput(double output, State setpoint) {
+        double feedforward =  feedforward.calculate(setpoint.position, setpoint.velocity);
+        ArmMotor1.setVoltage(output + feedforward);
+        ArmMotor2.set(output + feedforward);
     }
-    public void liftArm(double speed){
-        ArmMotor1.set(speed);
-        ArmMotor2.set(speed);
-    }
-    public void liftArm(){
-        ArmMotor1.set(ArmConstants.ArmLiftSpeed);
-        ArmMotor2.set(ArmConstants.ArmLiftSpeed);
-    }
-    public void lowerArm(double speed){
-        ArmMotor1.set(-speed);
-        ArmMotor2.set(-speed);
-    }
-    public void lowerArm(){
-        ArmMotor1.set(ArmConstants.ArmLowerSpeed);
-        ArmMotor2.set(ArmConstants.ArmLowerSpeed);
-    }
-    public void stopArm(){
-        ArmMotor1.set(0);
-        ArmMotor2.set(0);
+    @Override
+    protected double getMeasurement() {
+        // TODO Auto-generated method stub
+        return 0;
     }
 }

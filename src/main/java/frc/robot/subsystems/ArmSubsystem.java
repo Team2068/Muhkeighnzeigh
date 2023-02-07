54a7b1983c2beb154a70f6631ab988c660597039
keiraSchoolcraft <schineaj23@pwcs-edu.org>
2023-02-07 17:08:14 -0500

    Added trapezoid and profile

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,45 +1,22 @@
 package frc.robot.subsystems;
 
-import java.beans.Encoder;
-
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
-import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 
-public class ArmSubsystem extends ProfiledPIDSubsystem{
-    private CANSparkMax ArmMotor1 = new CANSparkMax(ArmConstants.ArmMotor1, MotorType.kBrushless);
-    private CANSparkMax ArmMotor2 = new CANSparkMax(ArmConstants.ArmMotor2, MotorType.kBrushless);
-    private Encoder Arm1encoder;
-    private Encoder arm2Encoder;
-    private final ArmFeedforward armFeedforward = 
-    new ArmFeedforward(0, 0, 0, 0);
-    public ArmSubsystem(){
-   super(
-    new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(0, 0))
-   );
-
-Arm1encoder.setDistancePerPulse(0);
-arm2Encoder.setDistancePerPulse(0);
-setGoal(0);
-}
-    @Override
-    protected void useOutput(double output, State setpoint) {
-        double feedforward =  feedforward.calculate(setpoint.position, setpoint.velocity);
-        ArmMotor1.setVoltage(output + feedforward);
-        ArmMotor2.setVoltage(output + feedforward);
-    }
-    @Override
-    protected double getMeasurement() {
-        // TODO Auto-generated method stub
-        return arm2Encoder.getDistance() + ARMOFFSET;
-        return Arm1encoder.getDistance() + ARMOFFSET;
-    }
+public class ArmSubsystem extends SubsystemBase{
+    private static double kDt = 0;
+    private final Encoder armEncoder = new Encoder(1, 2);
+    private final CANSparkMax arm1Motor = new CANSparkMax(ArmConstants.ArmMotor1, MotorType.kBrushless);
+    private final CANSparkMax arm2Motor = new CANSparkMax(ArmConstants.ArmMotor2, MotorType.kBrushless);
+    private final TrapezoidProfile.Constraints constraints =
+        new TrapezoidProfile.Constraints(0, 0);
+    private final ProfiledPIDController controller =
+        new ProfiledPIDController(0,0,0, constraints, kDt);
 }

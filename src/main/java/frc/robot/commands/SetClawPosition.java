e5839a58a297db7a35cba90ce2f54aacd100faee
Andrew Schineller <schineaj23@pwcs-edu.org>
2023-03-02 15:24:09 -0500

    SetClawPos: Removed arbitrary conversion factor

--- src/main/java/frc/robot/commands/SetClawPosition.java
+++ src/main/java/frc/robot/commands/SetClawPosition.java
@@ -16,7 +16,6 @@ public class SetClawPosition extends CommandBase {
         clawController.setSetpoint(angleDegrees);
         clawController.setTolerance(1);
         clawController.enableContinuousInput(0, 360);
-
     }
 
     @Override
@@ -31,7 +30,7 @@ public class SetClawPosition extends CommandBase {
         double clawPidOutput = clawController.calculate(currentClawPosition);
         double cffOutput = clawSubsystem.calculateClawFeedforward(Math.toRadians(clawSetpoint),
                 (Math.toRadians(currentClawPosition) - Math.toRadians(lastClawPosition)) / clawController.getPeriod());
-        double newClawOutput = (clawPidOutput / 180 * 8) + cffOutput;
+        double newClawOutput = clawPidOutput + cffOutput;
         SmartDashboard.putNumber("Claw PID", clawPidOutput);
         SmartDashboard.putNumber("Claw FeedForwad", cffOutput);
         SmartDashboard.putNumber("Claw Voltage", newClawOutput);
@@ -44,6 +43,7 @@ public class SetClawPosition extends CommandBase {
     @Override
     public void end(boolean interrupted) {
         clawSubsystem.setIntakeSpeed(0);
+        clawSubsystem.setWristSpeed(0);
     }
 
     @Override

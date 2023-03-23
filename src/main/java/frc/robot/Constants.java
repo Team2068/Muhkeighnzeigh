a42a618330be7b33ad61bf00b7d75ebd348cdfe8
Andrew Schineller <66565336+TotallyFarhan@users.noreply.github.com>
2023-03-23 18:39:11 -0400

    offset update

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -71,7 +71,7 @@ public final class Constants {
 
     public static final void setOffsets() { 
       if (Constants.getChassisConfiguration() == ChassisConfiguration.MAIN) {
-        FRONT_LEFT_ENCODER_OFFSET = -Math.toRadians(133);
+        FRONT_LEFT_ENCODER_OFFSET = -Math.toRadians(85);
         FRONT_RIGHT_ENCODER_OFFSET = -Math.toRadians(100);
         BACK_LEFT_ENCODER_OFFSET = -Math.toRadians(164);
         BACK_RIGHT_ENCODER_OFFSET = -Math.toRadians(44);
